1b015f7aceec660282f311ee82e4a04f5546bf89
dhruva1995 <helloworldiamnew@gmail.com>
2023-06-19 23:36:53 +0530

    Inital commit

---
+++ src/main/java/dc/typeface/titanicquery/TitanicQueryApplication.java
@@ -0,0 +1,13 @@
+package dc.typeface.titanicquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanicQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanicQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dc/typeface/titanicquery/TitanicQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package dc.typeface.titanicquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
